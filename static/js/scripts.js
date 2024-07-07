22dfa28a5c97213a9153eaae559b3bb44b07d0b3
= <-rjaiswar257@gmail.com>
2024-07-07 20:51:38 +0530

    added some code to prevebt from downloading videos

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,9 @@
   document.addEventListener('contextmenu', function (e) {
-    if (e.target.tagName === 'IMG', 'video') {
+    if (e.target.tagName === 'IMG') {
       e.preventDefault();
     }
   });
 
-  
+  document.getElementById('video').addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
