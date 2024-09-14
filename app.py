9b8e45f7c661d7949ebdd66c4532520af714b72a
= <-rjaiswar257@gmail.com>
2024-09-14 13:32:28 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+from flask_caching import Cache
 
 app = Flask(__name__)
 
+# Configuring Cache
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
+
 @app.route('/')
+@cache.cached(timeout=60*60)  # Cache for 1 hour
 def home():
     return render_template('index.html')
 
