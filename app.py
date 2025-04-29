5ca79bf686bcc999bd52e7f27c5f631bc9b09c2b
rahul jaiswar <rjaiswar257@gmail.com>
2025-04-29 13:15:19 +0530

    Added some new projects and skills and improve the design a little bit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import data
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', skills=data.skills, projects=data.projects, certificates=data.certificates)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,83 @@
+skills = {
+    "programming_languages": ["Python", "JavaScript", "SQL"],
+    "data_manipulation": ["Pandas", "NumPy", "Excel", "Advanced Excel"],
+    "data_visualization": ["Matplotlib", "Seaborn", "Power BI", "Google Looker Studio"],
+    "databases": ["MySQL", "SQL Queries"],
+    "aws_tools": ["S3", "Athena", "Glue", "Glue DataBrew", "Quicksight"],
+    "web_development": ["HTML", "CSS", "Bootstrap"],
+    "frameworks": ["Flask", "Streamlit"],
+    "tools": [
+        "Google Sheets", "Jupyter Notebook", "Google Collab",
+        "Microsoft Word", "PowerPoint", "GitHub", "Google Workspace"
+    ],
+    "other_skills": [
+        "Data Cleaning", "Data Wrangling", "Data Visualization",
+        "Exploratory Data Analysis (EDA)", "Statistical Analysis", "Dashboarding"
+    ],
+    "soft_skills": [
+        "Problem-Solving", "Communication", "Collaborative Team Player", "Attention to Details"
+    ]
+}
+
+projects = [
+    {
+        "title": "Exploratory Data Analysis on Diwali Purchase Trends",
+        "tools": ["Python", "Pandas", "NumPy", "Matplotlib", "Seaborn"],
+        "description": "Analyzed customer segments, top-performing products, and gender-based buying trends. Derived insights for targeted marketing.",
+        "link": "https://colab.research.google.com/drive/1mjuCz6xtx3F9o3RlUUUIBiiQimzdKf8E?usp=sharing",
+        "image": "eda.jpg"
+    },
+    {
+        "title": "E-Commerce Dashboard | Power BI",
+        "tools": ["Power BI", "Python"],
+        "description": "Built interactive dashboard for platforms like Blinkit, JioMart, and Swiggy Instamart. Integrated Python word cloud and custom filtering with bookmarks.",
+        "link": "https://github.com/rahuljaiswar/ecom-dash",
+        "image": "pbi.png"
+    }
+,
+    {
+        'title': 'Amazon Sales Data Analysis', 
+        'tools': ['Python', 'pandas', 'matplotlib', 'seaborn'], 
+        'description': 'Identified key trends and business improvement areas. Created visualizations with matplotlib and seaborn to support data-driven decisions.', 
+        'link': 'https://colab.research.google.com/drive/1T2Y7lEeiH7TAwjhKcvsW9weeF2Q_bAgr?usp=sharing',
+        "image": "amz.png"
+    },
+        
+    {
+        'title': 'Power BI Project (Financial Reports)', 
+        'tools': ['Power BI'], 
+        'description': 'Designed a Power BI Financial Reports dashboard to understand goods sales and display trends for informed decision-making.', 
+        'link': 'https://github.com/rahuljaiswar/Power_bi_financial_dashboard',
+        "image": "pbi.png"
+    },
+        
+    {
+        'title': 'A Painter Portfolio', 
+        'tools': ['HTML', 'CSS', 'JavaScript', 'Bootstrap'], 
+        'description': 'Designed a portfolio website for a painter.', 
+        'link': 'https://rahuljaiswar.github.io/ajay/',
+        "image": "painter.png"
+        
+    }
+    ]
+
+certificates = [
+    {
+        "name": "Power BI",
+        "provider": "Simplilearn",
+        "link": "https://www.simplilearn.com/skillup-certificate-landing?token=eyJjb3Vyc2VfaWQiOiIxNzIyIiwiY2VydGlmaWNhdGVfdXJsIjoiaHR0cHM6XC9cL2NlcnRpZmljYXRlcy5zaW1wbGljZG4ubmV0XC9zaGFyZVwvdGh1bWJfNDA1NDkwNl8xNjcyNDczMDIyLnBuZyIsInVzZXJuYW1lIjoiUmFodWwgamFpc3dhciJ9",
+        "image": "PBI.png"
+    },
+    {
+        "name": "Pandas",
+        "provider": "Kaggle",
+        "link": "https://drive.google.com/file/d/1dKk_ix4zp5x1p9-8hRtKUejtifrqKm36/view",
+        "image": "pandas.svg"
+    },
+    {
+        "name": "SQL For Data Science",
+        "provider": "Great Learning",
+        "link": "https://drive.google.com/file/d/1WS0zR8Zplbo6GZ2l1RKhu5QWKUmoe3ta/view",
+        "image": "sql.png"
+    }
+]
